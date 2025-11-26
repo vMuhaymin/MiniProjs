e6226c47ff5a4f2dec6b1fe730859752622cced3
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2025-11-26 21:39:20 +0300

    Update function has been added

--- DailyNotion/DailyNotion/Notion/urls.py
+++ DailyNotion/DailyNotion/Notion/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("" , views.viewTable, name="viewTable"),
     path("add/" , views.addForm_view, name="newReport"),
+    path("" , views.viewTable, name="viewTable"),
+    path("update/<int:id>" , views.updateTable , name = "updateTable"),
+    path("delete/<int:id>" , views.deleteInfo, name = "deleteInfo")
+
 
 ]
--- DailyNotion/DailyNotion/Notion/views.py
+++ DailyNotion/DailyNotion/Notion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
 from .forms import NoteForm
 from .models import Note
 # Create your views here.
@@ -8,7 +8,7 @@ def addForm_view(request):
         form = NoteForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse("Success")
+            return redirect('/')
 
     return render(request, "addReport.html", {"form":form})
 
@@ -16,3 +16,24 @@ def addForm_view(request):
 def viewTable(request):
     form = Note.objects.all()
     return render(request, "StudyReport.html", {'notes' : form})
+
+def updateTable(request, id ):
+    item = Note.objects.get(id = id)
+
+    if request.method == "GET" and request.GET :
+        form= NoteForm(request.GET , instance= item )
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = NoteForm(instance = item)
+    return render(request, 'update.html', {'form': form} )
+
+def deleteInfo(request, id):
+    item= Note.objects.get(id = id)
+    form = NoteForm(instance = item)
+    if request.method =="POST":
+        item.delete()
+        return redirect('/')
+    return render(request, 'delete.html', {"form": form} )
+
