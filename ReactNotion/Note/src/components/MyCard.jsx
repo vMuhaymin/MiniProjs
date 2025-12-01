42804f4f310dbcd099f7437b093ef6d3f2c1bbad
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2025-12-01 14:05:50 +0300

    Card styling

---
+++ ReactNotion/Note/src/components/MyCard.jsx
@@ -0,0 +1,18 @@
+
+function MyCard(){
+    return(<>
+    <div className="card-container">
+            <ul className="main-Info-data"> 
+                {/* Require A fix, its table data <td> not <li>  */}
+                <li>Day</li>
+                <li>Course</li>
+                <li>Total Time</li>
+                <li> Material </li>
+            </ul>
+
+        
+    </div>
+    </>);
+}
+
+export default MyCard;
